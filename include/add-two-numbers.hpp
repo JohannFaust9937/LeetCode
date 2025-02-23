eac56c073498a7db926263abc50e234d2662c01f
JohannFaust9937 <zcQwMLsu@protonmail.com>
2025-02-23 18:05:49 +0300

    Update of solved problems...

--- include/add-two-numbers.hpp
+++ include/add-two-numbers.hpp
@@ -5,7 +5,8 @@
 // https://leetcode.com/problems/add-two-numbers/description/
 
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 */
@@ -22,13 +23,24 @@ struct ListNode
 class Solution
 {
 public:
-    Solution() {};
-    ~Solution() = default;
-
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    ListNode *addTwoNumbers(ListNode *list1, ListNode *list2)
     {
+        ListNode *temporary = new ListNode();
+        ListNode *result = temporary;
+
+        int carry{0};
+        while (list1 != nullptr or list2 != nullptr or carry != 0)
+        {
+            int amount = carry;
+            if (list1 != nullptr) { amount += list1->val; list1 = list1->next; }
+            if (list2 != nullptr) { amount += list2->val; list2 = list2-> next; }
+            carry = amount / 10;
+            result->next = new ListNode(amount % 10);
+            result = result->next;
+        }
+        return temporary->next;
     }
 };
 
-// Temporal complexity: O(?)
-// Spatial complexity: O(?)
+// Temporal complexity: O(n)
+// Spatial complexity: O(1)
